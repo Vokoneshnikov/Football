869734b75559c9ccfe0e9514fd31bd5856498351
Vladimir Okonesnikov <okoneshnikov.vova@gmail.com>
2024-12-31 15:17:59 +0300

    Измененный MD файл

--- Football/Football/Program.cs
+++ Football/Football/Program.cs
@@ -1,1 +1,43 @@
-﻿
+﻿interface ISpeakable
+{ 
+    public void SaySpeech();
+}
+
+public abstract class FootballMatchParticipant : ISpeakable
+{
+    public abstract void SaySpeech();
+    public abstract void ShowStatistics();
+
+}
+
+public class Footballer : FootballMatchParticipant
+{
+    public override void SaySpeech()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void ShowStatistics()
+    {
+        throw new NotImplementedException();
+    }
+}
+abstract class VoiceAssistant : ISpeakable
+{
+    public abstract void SaySpeech();
+}
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        FootballMatchParticipant participant = new FootballMatchParticipant();
+        
+        
+        
+        
+        
+    }
+}
